9db46c57b15bd042dcaa3f709a47b57b65c49860
Yre6 <irene.conti@araig.it>
2022-06-28 15:36:25 +0200

    creata relazione tra tour e environment

--- app/Models/Tour.php
+++ app/Models/Tour.php
@@ -9,4 +9,8 @@ class Tour extends Model
     public function valley(){
         return $this->belongsTo('App\Models\Valley');
     }
+
+    public function environment(){
+        return $this->belongsto('App\Models\Environment');
+    }
 }
