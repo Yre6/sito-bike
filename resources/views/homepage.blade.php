e7196e4839cbe91b30d50bf58fc088527f665de8
Yre6 <irene.conti@araig.it>
2022-06-29 19:32:34 +0200

    impostato header e main hompage

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,5 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>HOMEPAGE</h1>
+<div id="homepage-main" class="d-flex">
+    <div class="col-6 d-flex justify-content-center">
+        <div class="w-50 m-auto d-flex flex-column justify-content-center">
+            <h1 class="mb-3">Araig <br> Mountain Bike
+            Santorini</h1>
+            <h5 class="fw-normal mb-5">
+            Amore per la montagna, passione per le due ruote.
+            </h5>
+
+            <div class="arrow-button position-relative">
+                <button>
+                    <a href="">SCOPRI DI PI&Ugrave;</a>
+                </button>   
+                <img class="position-absolute " src="{{asset('img/arrow.png')}}" alt="">
+            </div>
+        </div>
+       
+    </div>
+    <div class="col-6">
+        <img class="w-100 h-100" src="{{asset('img/jumbo.jpg')}}" alt="">
+    </div>
+
+</div>
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,12 @@
-<h1>HEADER</h1>
+<header>
+    <div class="w-75 m-auto d-flex justify-content-between">
+        <div> LOGO - MENU?</div>
+        <div class="arrow-button position-relative">
+            <button>
+                <a href="">RIMANI AGGIORNATO</a>
+            </button>   
+            <img class="position-absolute " src="{{asset('img/arrow.png')}}" alt="">
+        </div>
+        
+    </div>
+</header>
